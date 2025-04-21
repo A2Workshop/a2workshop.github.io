1a50fdf08293b508cd3b2174850416c0ec2dd6f0
artur16211 <herrera.alvaradoartu@gmail.com>
2025-04-21 10:42:09 -0600

    updt

--- assets/js/gennav.js
+++ assets/js/gennav.js
@@ -19,9 +19,6 @@
                     <a href="/traducciones/pq2espanol.html">
                         <img src="../pq2/img/es_logo/blackbg.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
                     </a>
-                    <a href="/traducciones/p4despanol.html">
-                        <img src="../p4d/img/es_logo/blackbg.png" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
-                    </a>
                     </div>
                 </div>
                 <a href="/contacto.html" class="contacto-hover">Contacto</a>
@@ -46,9 +43,6 @@
                     </a>
                     <a href="/traducciones/pq2espanol.html">
                         <img src="/pq2/img/es_logo/blackbg.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                    </a>
-                    <a href="/traducciones/p4despanol.html">
-                        <img src="/p4d/img/es_logo/blackbg.png" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
                     </a>
                     </div>
                 </div>
@@ -77,9 +71,6 @@
                     <a href="/traducciones/pq2espanol.html">
                         <img src="pq2/img/es_logo/blackbg.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
                     </a>
-                    <a href="/traducciones/p4despanol.html">
-                        <img src="p4d/img/es_logo/blackbg.png" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
-                    </a>
                     </div>
                 </div>
                 <a href="/contacto.html" class="contacto-hover">Contacto</a>
@@ -106,9 +97,6 @@
                     </a>
                     <a href="/traducciones/pq2espanol.html">
                         <img src="../../pq2/img/es_logo/blackbg.png" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                    </a>
-                    <a href="/traducciones/p4despanol.html">
-                        <img src="../../p4d/img/es_logo/blackbg.png" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
                     </a>
                     <hr class="nav-hr">
                     <a href="https://p3pespanol.wordpress.com/2024/05/04/persona-q-parche-en-espanol/">
