3f80ed9d6114564481f582d3098e1fdf9367bc3d
artur16211 <herrera.alvaradoartu@gmail.com>
2025-05-29 17:56:16 -0600

    update

--- assets/js/gennav.js
+++ assets/js/gennav.js
@@ -12,19 +12,19 @@
                             <img src="../assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
                             Latino
                         </a>
-                        <a href="/proyectos/blackopslatino.html">
+                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
                             <img src="../assets/img/BO1/logo.webp" alt="bolalogo">
                             (PC) Call of Duty: Black Ops Zombies Español Latino
                         </a>
-                        <a href="/proyectos/infinitewarfarelatino.html">
+                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
                             <img src="../assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
                             (PC) Call of Duty: Infinite Warfare Español Latino
                         </a>
-                        <a href="/proyectos/modernwarfarerlatino.html">
+                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
                             <img src="../assets/img/MWR/MWR.webp" alt="mwlalogo">
                             (PC) Call of Duty: Modern Warfare Remastered Español Latino
                         </a>
-                        <a href="/traducciones/pq2espanol.html">
+                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
                             <img src="../pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
                         </a>
                     </div>
@@ -45,19 +45,19 @@
                             <img src="/assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
                             Latino
                         </a>
-                        <a href="/proyectos/blackopslatino.html">
+                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
                             <img src="/assets/img/BO1/logo.webp" alt="bolalogo">
                             (PC) Call of Duty: Black Ops Zombies Español Latino
                         </a>
-                        <a href="/proyectos/infinitewarfarelatino.html">
+                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
                             <img src="/assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
                             (PC) Call of Duty: Infinite Warfare Español Latino
                         </a>
-                        <a href="/proyectos/modernwarfarerlatino.html">
+                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
                             <img src="/assets/img/MWR/MWR.webp" alt="mwlalogo">
                             (PC) Call of Duty: Modern Warfare Remastered Español Latino
                         </a>
-                        <a href="/traducciones/pq2espanol.html">
+                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
                             <img src="/pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
                         </a>
                     </div>
@@ -80,19 +80,19 @@
                             <img src="assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
                             Latino
                         </a>
-                        <a href="/proyectos/blackopslatino.html">
+                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
                             <img src="assets/img/BO1/logo.webp" alt="bolalogo">
                             (PC) Call of Duty: Black Ops Zombies Español Latino
                         </a>
-                        <a href="/proyectos/infinitewarfarelatino.html">
+                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
                             <img src="assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
                             (PC) Call of Duty: Infinite Warfare Español Latino
                         </a>
-                        <a href="/proyectos/modernwarfarerlatino.html">
+                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
                             <img src="assets/img/MWR/MWR.webp" alt="mwlalogo">
                             (PC) Call of Duty: Modern Warfare Remastered Español Latino
                         </a>
-                        <a href="/traducciones/pq2espanol.html">
+                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
                             <img src="pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
                         </a>
                     </div>
@@ -115,30 +115,30 @@
                             <img src="../../assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
                             Latino
                         </a>
-                        <a href="/proyectos/blackopslatino.html">
+                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
                             <img src="../../assets/img/BO1/logo.webp" alt="bolalogo">
                             (PC) Call of Duty: Black Ops Zombies Español Latino
                         </a>
-                        <a href="/proyectos/infinitewarfarelatino.html">
+                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
                             <img src="../../assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
                             (PC) Call of Duty: Infinite Warfare Español Latino
                         </a>
-                        <a href="/proyectos/modernwarfarerlatino.html">
+                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
                             <img src="../../assets/img/MWR/MWR.webp" alt="mwlalogo">
                             (PC) Call of Duty: Modern Warfare Remastered Español Latino
                         </a>
-                        <a href="/traducciones/pq2espanol.html">
+                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
                             <img src="../../pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
                         </a>
-                        <a href="/traducciones/p4despanol.html">
+                        <a href="/traducciones/p4despanol.html" class="disable-padding-top">
                             <img src="../../p4d/img/es_logo/blackbg.webp" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
                         </a>
                         <hr class="nav-hr">
-                        <a href="https://gamebanana.com/mods/54473">
+                        <a href="https://gamebanana.com/mods/54473" class="disable-padding-top">
                             <img src="../../pq2/img/es_logo/p5_ps3.jpg" alt="p5ps3logo"> (PS3) Persona 5 Backport
                                     Español
                         </a>
-                        <a href="https://gamebanana.com/mods/511619">
+                        <a href="https://gamebanana.com/mods/511619" class="disable-padding-top">
                             <img src="../../pq2/img/es_logo/blackbg_q1.webp" alt="pqeslogo"> (3DS) Persona Q Traducción al Español
                         </a>
                     </div>
@@ -161,30 +161,30 @@
                             <img src="../../assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
                             Latino
                         </a>
-                        <a href="/proyectos/blackopslatino.html">
+                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
                             <img src="../../assets/img/BO1/logo.webp" alt="bolalogo">
                             (PC) Call of Duty: Black Ops Zombies Español Latino
                         </a>
-                        <a href="/proyectos/infinitewarfarelatino.html">
+                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
                             <img src="../../assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
                             (PC) Call of Duty: Infinite Warfare Español Latino
                         </a>
-                        <a href="/proyectos/modernwarfarerlatino.html">
+                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
                             <img src="../../assets/img/MWR/MWR.webp" alt="mwlalogo">
                             (PC) Call of Duty: Modern Warfare Remastered Español Latino
                         </a>
-                        <a href="/traducciones/pq2espanol.html">
+                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
                             <img src="../../pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
                         </a>
-                        <a href="/traducciones/p4despanol.html">
+                        <a href="/traducciones/p4despanol.html" class="disable-padding-top">
                             <img src="../../p4d/img/es_logo/blackbg.webp" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
                         </a>
                         <hr class="nav-hr">
-                        <a href="https://gamebanana.com/mods/54473">
+                        <a href="https://gamebanana.com/mods/54473" class="disable-padding-top">
                             <img src="../../pq2/img/es_logo/p5_ps3.jpg" alt="p5ps3logo"> (PS3) Persona 5 Backport
                                     Español
                         </a>
-                        <a href="https://gamebanana.com/mods/511619">
+                        <a href="https://gamebanana.com/mods/511619" class="disable-padding-top">
                             <img src="../../pq2/img/es_logo/blackbg_q1.webp" alt="pqeslogo"> (3DS) Persona Q Traducción al Español
                         </a>
                     </div>
