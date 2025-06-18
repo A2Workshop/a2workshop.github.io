186f74f0b2e0beeb68d201e7c3a7058ca36f9078
artur16211 <herrera.alvaradoartu@gmail.com>
2025-06-18 04:45:47 -0600

    update

--- assets/js/gennav.js
+++ assets/js/gennav.js
@@ -1,223 +1,119 @@
-﻿function generarNavbar() {
-    const navbar = `
-        <nav>
-            <div class="nav-container">
-                <a href="/" class="hide-desktop2"><img src="../assets/img/A2/logo.webp" alt="logo" class="logo_nav"></a>
-                <a href="/" class="hide-mobile2"><img src="../assets/img/A2/full_logo.webp" alt="logo" class="logo_nav"></a>
-                <div class="megamenu">
-                    <a href="#">Proyectos</a>
-                    <div class="megamenu-content">
-                        <button class="close-btn">&times;</button>
-                        <a href="/proyectos/blackopsiiilatino.html">
-                            <img src="../assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
-                            Latino
-                        </a>
-                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
-                            <img src="../assets/img/BO1/logo.webp" alt="bolalogo">
-                            (PC) Call of Duty: Black Ops Zombies Español Latino
-                        </a>
-                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
-                            <img src="../assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
-                            (PC) Call of Duty: Infinite Warfare Español Latino
-                        </a>
-                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
-                            <img src="../assets/img/MWR/MWR.webp" alt="mwlalogo">
-                            (PC) Call of Duty: Modern Warfare Remastered Español Latino
-                        </a>
-                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
-                            <img src="../pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                        </a>
-                        <a href="/traducciones/p4despanol.html" class="disable-padding-top">
-                            <img src="../p4d/img/es_logo/blackbg.webp" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
-                        </a>
-                    </div>
-                </div>
-                <a href="/contacto.html" class="contacto-hover">Contacto</a>
-            </div>
-        </nav>
+﻿// prefix
+const navbarConfigs = {
+  default: {
+    prefix: '../',
+    extraContainerClass: '',
+    showLogo: true,
+    logoPath: 'assets/img/A2/logo.webp',
+    fullLogoPath: 'assets/img/A2/full_logo.webp'
+  },
+  index: {
+    prefix: '/',
+    extraContainerClass: 'p-2',
+    showLogo: false
+  },
+  contacto: {
+    prefix: '',
+    extraContainerClass: '',
+    showLogo: true,
+    logoPath: 'assets/img/A2/logo.webp',
+    fullLogoPath: 'assets/img/A2/full_logo.webp'
+  },
+  pq2install: {
+    prefix: '../../../',
+    extraContainerClass: '',
+    showLogo: true,
+    logoPath: 'pq2/img/a2logo/logo.webp',
+    fullLogoPath: 'pq2/img/a2logo/full_logo.webp'
+  },
+  p4d: {
+    prefix: '../../',
+    extraContainerClass: '',
+    showLogo: true,
+    logoPath: 'assets/img/A2/logo.webp',
+    fullLogoPath: 'assets/img/A2/full_logo.webp',
+    boldProyecto: true,
+    boldContacto: true
+  }
+};
+
+// paths
+const routesMap = {
+  '/': 'index',
+  '/index': 'index',
+  '/index.html': 'index',
+  '/contacto': 'contacto',
+  '/contacto.html': 'contacto',
+  '/traducciones/pq2espanol/descargar': 'pq2install',
+  '/traducciones/pq2espanol/descargar.html': 'pq2install',
+  '/traducciones/p4despanol': 'p4d',
+  '/traducciones/p4despanol.html': 'p4d',
+};
+
+// items
+const menuItems = [
+  { href: '/proyectos/blackopsiiilatino.html', imgPath: 'assets/img/BO3/logo.webp', alt: 'bo3lalogo', text: '(PC) Call of Duty: Black Ops III Español Latino' },
+  { href: '/proyectos/blackopslatino.html',    imgPath: 'assets/img/BO1/logo.webp', alt: 'bolalogo', text: '(PC) Call of Duty: Black Ops Zombies Español Latino' },
+  { href: '/proyectos/infinitewarfarelatino.html', imgPath: 'assets/img/IW/images.steamusercontent.jpeg', alt: 'iwlalogo', text: '(PC) Call of Duty: Infinite Warfare Español Latino' },
+  { href: '/proyectos/modernwarfarerlatino.html', imgPath: 'assets/img/MWR/MWR.webp', alt: 'mwlalogo', text: '(PC) Call of Duty: Modern Warfare Remastered Español Latino' },
+  { href: '/traducciones/pq2espanol.html', imgPath: 'pq2/img/es_logo/blackbg.webp', alt: 'pq2eslogo', text: '(3DS) Persona Q2 Traducción al Español' },
+  { href: '/traducciones/p4despanol.html', imgPath: 'p4d/img/es_logo/blackbg.webp', alt: 'p4deslogo', text: '(PS4/PSV) Persona 4 Dancing Traducción al Español' }
+];
+
+function generarNavbar() {
+  const ruta = window.location.pathname;
+  const key = routesMap[ruta] || 'default';
+  const config = navbarConfigs[key];
+  const { prefix, extraContainerClass, showLogo, logoPath, fullLogoPath } = config;
+
+  const itemsHTML = menuItems.map((item, i) => `
+    <a href="${item.href}"${i > 0 ? ' class="disable-padding-top"' : ''}>
+      <img src="${prefix}${item.imgPath}" alt="${item.alt}"> ${item.text}
+    </a>
+  `).join('');
+
+  const proyectoTexto = config.boldProyecto ? '<b>Proyectos</b>' : 'Proyectos';
+  const contactoHTML = config.boldContacto
+    ? '<a href="/contacto.html" class="contacto-hover"><b>Contacto</b></a>'
+    : '<a href="/contacto.html" class="contacto-hover">Contacto</a>';
+
+  let logosHTML = '';
+  if (showLogo) {
+    logosHTML = `
+      <a href="/" class="hide-desktop2">
+        <img src="${prefix}${logoPath}" alt="A2W" class="logo_nav">
+      </a>
+      <a href="/" class="hide-mobile2">
+        <img src="${prefix}${fullLogoPath}" alt="A2WORKSHOP" class="logo_nav">
+      </a>
     `;
+  }
 
-    const navbarindex = `
-        <nav>
-            <div class="nav-container p-2">
-                <div class="megamenu">
-                    <a href="#">Proyectos</a>
-                    <div class="megamenu-content">
-                        <button class="close-btn">&times;</button>
-                        <a href="/proyectos/blackopsiiilatino.html">
-                            <img src="/assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
-                            Latino
-                        </a>
-                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
-                            <img src="/assets/img/BO1/logo.webp" alt="bolalogo">
-                            (PC) Call of Duty: Black Ops Zombies Español Latino
-                        </a>
-                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
-                            <img src="/assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
-                            (PC) Call of Duty: Infinite Warfare Español Latino
-                        </a>
-                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
-                            <img src="/assets/img/MWR/MWR.webp" alt="mwlalogo">
-                            (PC) Call of Duty: Modern Warfare Remastered Español Latino
-                        </a>
-                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
-                            <img src="/pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                        </a>
-                        <a href="/traducciones/p4despanol.html" class="disable-padding-top">
-                            <img src="/p4d/img/es_logo/blackbg.webp" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
-                        </a>
-                    </div>
-                </div>
-                <a href="/contacto.html" class="contacto-hover">Contacto</a>
-            </div>
-        </nav>
-    `;
+  const html = `
+    <nav>
+      <div class="nav-container ${extraContainerClass}">
+        ${logosHTML}
+        <div class="megamenu">
+          <a href="#">${proyectoTexto}</a>
+          <div class="megamenu-content">
+            <button class="close-btn">&times;</button>
+            ${itemsHTML}
+          </div>
+        </div>
+        ${contactoHTML}
+      </div>
+    </nav>
+  `;
 
-    const navbarcontacto = `
-        <nav>
-            <div class="nav-container">
-                <a href="/" class="hide-desktop2"><img src="assets/img/A2/logo.webp" alt="logo" class="logo_nav"></a>
-                <a href="/" class="hide-mobile2"><img src="assets/img/A2/full_logo.webp" alt="logo" class="logo_nav"></a>
-                <div class="megamenu">
-                    <a href="#">Proyectos</a>
-                    <div class="megamenu-content">
-                        <button class="close-btn">&times;</button>
-                        <a href="/proyectos/blackopsiiilatino.html">
-                            <img src="assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
-                            Latino
-                        </a>
-                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
-                            <img src="assets/img/BO1/logo.webp" alt="bolalogo">
-                            (PC) Call of Duty: Black Ops Zombies Español Latino
-                        </a>
-                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
-                            <img src="assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
-                            (PC) Call of Duty: Infinite Warfare Español Latino
-                        </a>
-                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
-                            <img src="assets/img/MWR/MWR.webp" alt="mwlalogo">
-                            (PC) Call of Duty: Modern Warfare Remastered Español Latino
-                        </a>
-                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
-                            <img src="pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                        </a>
-                        <a href="/traducciones/p4despanol.html" class="disable-padding-top">
-                            <img src="p4d/img/es_logo/blackbg.webp" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
-                        </a>
-                    </div>
-                </div>
-                <a href="/contacto.html" class="contacto-hover">Contacto</a>
-            </div>
-        </nav>
-    `;
+  document.getElementById('navbar-container').innerHTML = html;
 
-    const navbarpq2install = `
-        <nav>
-            <div class="nav-container">
-                <a href="/" class="hide-desktop2"><img src="../../../pq2/img/a2logo/logo.webp" alt="logo" class="logo_nav"></a>
-                <a href="/" class="hide-mobile2"><img src="../../pq2/img/a2logo/full_logo.webp" alt="logo" class="logo_nav"></a>
-                <div class="megamenu">
-                    <a href="#">Proyectos</a>
-                    <div class="megamenu-content">
-                        <button class="close-btn">&times;</button>
-                        <a href="/proyectos/blackopsiiilatino.html">
-                            <img src="../../assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
-                            Latino
-                        </a>
-                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
-                            <img src="../../assets/img/BO1/logo.webp" alt="bolalogo">
-                            (PC) Call of Duty: Black Ops Zombies Español Latino
-                        </a>
-                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
-                            <img src="../../assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
-                            (PC) Call of Duty: Infinite Warfare Español Latino
-                        </a>
-                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
-                            <img src="../../assets/img/MWR/MWR.webp" alt="mwlalogo">
-                            (PC) Call of Duty: Modern Warfare Remastered Español Latino
-                        </a>
-                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
-                            <img src="../../pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                        </a>
-                        <a href="/traducciones/p4despanol.html" class="disable-padding-top">
-                            <img src="../../p4d/img/es_logo/blackbg.webp" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
-                        </a>
-                    </div>
-                </div>
-                <a href="/contacto.html" class="contacto-hover">Contacto</a>
-            </div>
-        </nav>
-    `;
-
-    const navbarp4d = `
-        <nav>
-            <div class="nav-container">
-                <a href="/" class="hide-desktop2"><img src="../../assets/img/A2/logo.webp" alt="logo" class="logo_nav"></a>
-                <a href="/" class="hide-mobile2"><img src="../../assets/img/A2/full_logo.webp" alt="logo" class="logo_nav"></a>
-                <div class="megamenu">
-                    <a href="#"><b>Proyectos</b></a>
-                    <div class="megamenu-content">
-                        <button class="close-btn">&times;</button>
-                        <a href="/proyectos/blackopsiiilatino.html">
-                            <img src="../../assets/img/BO3/logo.webp" alt="bo3lalogo"> (PC) Call of Duty: Black Ops III Español
-                            Latino
-                        </a>
-                        <a href="/proyectos/blackopslatino.html" class="disable-padding-top">
-                            <img src="../../assets/img/BO1/logo.webp" alt="bolalogo">
-                            (PC) Call of Duty: Black Ops Zombies Español Latino
-                        </a>
-                        <a href="/proyectos/infinitewarfarelatino.html" class="disable-padding-top">
-                            <img src="../../assets/img/IW/images.steamusercontent.jpeg" alt="iwlalogo">
-                            (PC) Call of Duty: Infinite Warfare Español Latino
-                        </a>
-                        <a href="/proyectos/modernwarfarerlatino.html" class="disable-padding-top">
-                            <img src="../../assets/img/MWR/MWR.webp" alt="mwlalogo">
-                            (PC) Call of Duty: Modern Warfare Remastered Español Latino
-                        </a>
-                        <a href="/traducciones/pq2espanol.html" class="disable-padding-top">
-                            <img src="../../pq2/img/es_logo/blackbg.webp" alt="pq2eslogo"> (3DS) Persona Q2 Traducción al Español
-                        </a>
-                        <a href="/traducciones/p4despanol.html" class="disable-padding-top">
-                            <img src="../../p4d/img/es_logo/blackbg.webp" alt="p4deslogo"> (PS4/PSV) Persona 4 Dancing Traducción al Español
-                        </a>
-                    </div>
-                </div>
-                <a href="/contacto.html" class="contacto-hover"><b>Contacto</b></a>
-            </div>
-        </nav>
-    `;
-
-    if (window.location.pathname === "/index.html" || window.location.pathname === "/" || window.location.pathname === "/index") {
-        document.getElementById("navbar-container").innerHTML = navbarindex;
-    }
-
-    else if (window.location.pathname === "/contacto.html" || window.location.pathname === "/contacto") {
-        document.getElementById("navbar-container").innerHTML = navbarcontacto;
-    }
-
-    else if (window.location.pathname === "/traducciones/pq2espanol/descargar.html" || window.location.pathname === "/traducciones/pq2espanol/descargar") {
-        document.getElementById("navbar-container").innerHTML = navbarpq2install;
-    }
-
-    else if (window.location.pathname === "/traducciones/p4despanol.html" || window.location.pathname === "/traducciones/p4despanol" || window.location.pathname === "/p4despanol.html" || window.location.pathname === "/p4despanol") {
-        document.getElementById("navbar-container").innerHTML = navbarp4d;
-    }
-
-    else {
-        document.getElementById("navbar-container").innerHTML = navbar;
-    }
-
-    $(document).ready(function () {
-        $('.megamenu > a').on('click', function (e) {
-            e.preventDefault();
-            $('.megamenu-content').fadeIn();
-        });
-
-        $('.close-btn').on('click', function () {
-            $('.megamenu-content').fadeOut();
-        });
-    });
-
+  $('.megamenu > a').off('click').on('click', function(e) {
+    e.preventDefault();
+    $('.megamenu-content').fadeIn();
+  });
+  $('.close-btn').off('click').on('click', function() {
+    $('.megamenu-content').fadeOut();
+  });
 }
 
 generarNavbar();
